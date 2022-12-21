d0b34599dd99bd1e7899a1c9d5a701edc88e0998
Maxime Francois <maximefrancois@MacBook-Pro-de-Maxime.local>
2022-12-21 15:57:02 +0100

    add navbar, homepage  & recipe controller and views

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $cooking_time = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCookingTime(): ?\DateTimeInterface
+    {
+        return $this->cooking_time;
+    }
+
+    public function setCookingTime(\DateTimeInterface $cooking_time): self
+    {
+        $this->cooking_time = $cooking_time;
+
+        return $this;
+    }
+}
