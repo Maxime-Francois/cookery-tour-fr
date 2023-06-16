3894f6aecfd89ae49208d174769fbf017292b51b
MaximeF <maximefrancois92@gmail.com>
2023-06-16 14:29:57 +0200

    fix tootltip bug 5

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,31 +1,6 @@
 // NAVBAR
 
-//toggle menu dropdown
 
-const dropdownBtn = document.getElementById("btn");
-const dropdownMenu = document.getElementById("dropdown");
-// const toggleArrow = document.getElementById("arrow");
-
-// Toggle dropdown function
-const toggleDropdown = function () {
-   dropdownMenu.classList.toggle("show");
-   // toggleArrow.classList.toggle("arrow");
-};
-
-
-// Toggle dropdown open/close when dropdown button is clicked
-dropdownBtn.addEventListener("click", function (e) {
-   e.stopPropagation();
-   toggleDropdown();
-});
-
-
-// Close dropdown when dom element is clicked
-document.documentElement.addEventListener("click", function () {
-   if (dropdownMenu.classList.contains("show")) {
-      toggleDropdown();
-   }
-});
 
 // Open and Close Navbar Menu
 const navbarMenu = document.getElementById("menu");
@@ -98,6 +73,34 @@ if (searchToggle && searchCancel) {
      
    }
    SVGToolTip()
+
+
+//toggle menu dropdown
+
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+// const toggleArrow = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown = function () {
+   dropdownMenu.classList.toggle("show");
+   // toggleArrow.classList.toggle("arrow");
+};
+
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+   e.stopPropagation();
+   toggleDropdown();
+});
+
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+   if (dropdownMenu.classList.contains("show")) {
+      toggleDropdown();
+   }
+});
 
 
 
